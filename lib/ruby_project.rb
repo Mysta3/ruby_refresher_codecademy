460fbf030a151f5dc994b6cdc61e6a9fb44ba9c7
Mysta3 <myshawnestallings@gmail.com>
2025-10-04 16:58:24 -0700

    fixed class and rubocop styling

--- lib/ruby_project.rb
+++ lib/ruby_project.rb
@@ -1,18 +1,11 @@
 # frozen_string_literal: true
 
-require_relative "ruby_project/version"
+require_relative `ruby_project/version`
 
-module RubyProject
-  class Error < StandardError; end
-
-  # Your code goes here...
-  def self.hello(name = "World")
-    "Hello, #{name}!"
-  end
-
-  class Computer
-    def initialize()
-      
-    end
+# This Computer class representa a basic model of a computer that can create documents by a user.
+class Computer
+  def initialize(username, password)
+    @username = username
+    @password = password
   end
 end
