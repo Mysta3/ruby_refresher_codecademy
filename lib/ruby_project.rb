6d757c4a027aa248e85e439434a311f93a6b0d38
Mysta3 <myshawnestallings@gmail.com>
2025-10-04 16:59:35 -0700

    added code for a user hash that stores user/pass

--- lib/ruby_project.rb
+++ lib/ruby_project.rb
@@ -4,8 +4,11 @@ require_relative `ruby_project/version`
 
 # This Computer class representa a basic model of a computer that can create documents by a user.
 class Computer
+  @@users = {}
   def initialize(username, password)
     @username = username
     @password = password
+    
+    @@users[username] = password
   end
 end
