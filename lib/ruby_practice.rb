d88ddeb5ad357949e108c25fd526c2e80dc5b295
Mysta3 <myshawnestallings@gmail.com>
2025-10-04 18:56:03 -0700

    added new file

---
+++ lib/ruby_practice.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class Account
+  attr_reader :name, :balance
+
+  def initialize(name, balance = 100)
+    @name = name
+    @balance = balance
+  end
+
+  public
+  def display_balance(pin_number)
+    if pin_number == pin
+      puts 'Balance: $#{@balance}'
+    else
+      pin_error
+    end
+  end
+
+  def withdraw(pin_number, amount)
+    if pin_number == pin
+      puts 'Withdrew #{amount}. New Balance: $#{@balance}'
+    else
+      pin_error
+    end
+  end
+
+  private
+  def pin
+    @pin = 1234
+  end
+
+  def pin_error
+    'Access denied: incorrect PIN.'
+  end
+end
--- lib/ruby_project.rb
+++ lib/ruby_project.rb
@@ -17,18 +17,22 @@ end
 ## This is a Dog class used for exploring public vs private methods
 
 class Dog
+  attr_accessor :name
+
   def initialize(name, breed)
     @name = name
     @breed = breed
   end
 
   public
+
   def bark
     puts 'Woof'
   end
 
   private
+
   def id
-    @id_number = 12345
+    @id_number = 123_45
   end
 end
