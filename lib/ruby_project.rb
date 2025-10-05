959883c61e79787eb9ebe6c537ee6e1728741635
Mysta3 <myshawnestallings@gmail.com>
2025-10-04 17:20:54 -0700

    added Dog class to explore prvate vs pub methd

--- lib/ruby_project.rb
+++ lib/ruby_project.rb
@@ -3,6 +3,7 @@
 require_relative `ruby_project/version`
 
 # This Computer class representa a basic model of a computer that can create documents by a user.
+
 class Computer
   @users = []
   def initialize(username, password)
@@ -10,5 +11,19 @@ class Computer
     @password = password
     @files = []
     @users[username] = password
+  end
+end
+
+## This is a Dog class used for exploring public vs private methods
+
+class Dog
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+
+  public
+  def bark
+    puts 'Woof'
   end
 end
